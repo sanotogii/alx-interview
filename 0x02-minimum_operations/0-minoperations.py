623825c6213848aff0846970b8704c33d595f054
sanotogii <nasaereh@gmail.com>
2024-08-09 00:19:05 +0200

    task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for calculating minimum operations to achieve n H characters
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
