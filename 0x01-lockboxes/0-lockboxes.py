8fd05ea3bc46e9f122375cb74e4e435a80189417
sanotogii <nasaereh@gmail.com>
2024-08-15 23:09:51 +0200

    task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -15,12 +15,12 @@ def canUnlockAll(boxes):
     unlocked = [False] * n
     unlocked[0] = True
     stack = [0]
-    
+
     while stack:
         box = stack.pop()
         for key in boxes[box]:
             if key < n and not unlocked[key]:
                 unlocked[key] = True
                 stack.append(key)
-    
+
     return all(unlocked)
