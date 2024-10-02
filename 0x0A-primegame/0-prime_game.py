78a1aacf242ef461737987eb538164fc93e50845
sanotogii <nasaereh@gmail.com>
2024-10-02 14:08:20 +0200

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,36 +5,38 @@ Prime Game
 
 
 def isWinner(x, nums):
-    def is_prime(n):
-        """Check if a number is prime"""
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+    """
+    Determines the winner of the Prime Game for multiple rounds.
 
-    def get_primes(n):
-        """Get all prime numbers up to n"""
-        return [i for i in range(2, n + 1) if is_prime(i)]
+    :param x: Number of rounds
+    :param nums: An array of n for each round
+    :return: Name of the player that won the most rounds,
+    or None if it's a tie
+    """
+    if not nums or x < 1:
+        return None
 
-    def play_game(n):
-        """Simulate a single game"""
-        primes = get_primes(n)
-        return len(primes) % 2 == 1
+    max_num = max(nums)
+
+    sieve = [True] * (max_num + 1)
+    sieve[0] = sieve[1] = False
+    for i in range(2, int(max_num**0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_num + 1, i):
+                sieve[j] = False
+
+    primes_up_to = [0] * (max_num + 1)
+    for i in range(2, max_num + 1):
+        primes_up_to[i] = primes_up_to[i - 1] + (1 if sieve[i] else 0)
 
     maria_wins = 0
-    ben_wins = 0
-
     for n in nums:
-        if play_game(n):
+        if primes_up_to[n] % 2 == 1:
             maria_wins += 1
-        else:
-            ben_wins += 1
 
-    if maria_wins > ben_wins:
+    if maria_wins > x / 2:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif maria_wins < x / 2:
         return "Ben"
     else:
         return None
